4a119f4005e8e6244d0a850c8d157eb26259cae4
Oleksii Kolyvayko <kolyvayko@gmail.com>
2018-10-10 15:49:35 +0300

    Done

---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strconv"
+	"math"
+	"strings"
+)
+
+func main(){
+	rows := flag.Int("rows", 0, "Rows count.")
+	columns := flag.Int("columns", 0, "Columns count.")
+	peoples := flag.Int("peoples", 0, "Peoples count.")
+
+	flag.Parse()
+	//fmt.Println(*rows)
+	//fmt.Println(*columns)
+	//fmt.Println(*peoples)
+	//fmt.Println(flag.Args())
+
+
+	num, err := findUnhappyPoints(*rows,*columns,*peoples)
+	fmt.Println("Count unheppi points: ",num)
+	if err != ""{fmt.Println(err)}
+
+}
+
+func findUnhappyPoints(rows, columns, peoples int) (num int, err string){
+	switch {
+	case rows <= 0:
+		return 0, "Rows not valid"
+	case columns <= 0:
+		return 0, "Columns not valid"
+	case peoples > rows*columns:
+		return 0, "Peoples count to match"
+	default:
+		_,num = findBestPermutation(rows,columns,peoples)
+		err = ""
+	}
+	return
+}
+
+func findBestPermutation(rows,columns,peoples int)(permutation string, count int){
+	if peoples ==0 {
+		return "", 0
+	}else{
+		permutations:=findAllPermitations(rows,columns,peoples)
+		count =rows*columns*2
+		for _, p:=range permutations{
+			c := countUnheppiPoints(rows,columns,p)
+
+			if c < count{
+				count = c
+				permutation = p
+			}
+		}
+		return
+	}
+}
+
+func countUnheppiPoints(rows,columns int, permutation string) (count int){
+
+	p:=make([]string, rows,rows)
+	for i:=0; i<=rows-1;i++{
+		p[i] = permutation[(i*columns):(i*columns+(columns))]
+	}
+	for i, item:=range p{
+		//fmt.Println(i)
+		if i == rows-1 {
+			for j := 0; j <= len(item)-2; j++ {
+				if string(item[j]) == "1" && string(item[j+1]) == "1" {
+					count++
+				}
+			}
+		}else{
+			for j:=0; j<=len(item)-2; j++{
+				if string(item[j]) == "1" && string(item[j+1]) == "1" {
+					count++
+				}
+				if string(item[j]) == "1" && string(p[i+1][j])=="1"{
+					count++
+				}
+			}
+			if string(p[i][columns-1]) == "1" && string(p[i+1][columns-1]) == "1" {
+				count++
+			}
+		}
+	}
+	return
+}
+
+func findAllPermitations(rows, columns, peoples int) (permutations []string){
+	roomsCount := rows*columns
+	for i := 0; i <= int(math.Pow(2, float64(roomsCount))); i++{
+		variant:= strconv.FormatInt(int64(i),2)
+		if len(variant)< rows*columns{
+			variant = strings.Repeat("0", rows*columns-len(variant))
+		}
+		if res:=checkCount(variant,peoples); res{
+			permutations = append(permutations, variant)
+			//fmt.Println(permutations)
+		}
+	}
+	return
+}
+func checkCount(item string, people int) (result bool){
+	count :=0
+	for _, elem:= range item{
+
+		if fmt.Sprintf("%c",elem) == "1"{
+			count +=1
+		}
+	}
+	if count == people{
+		result = true
+	}
+	return
+}
---
+++ main_test.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"testing"
+	"fmt"
+)
+
+type testdata struct {
+	values []int
+	result int
+}
+
+var tests = []testdata{
+	{  []int{5,2,8}, 7 }, {  []int{3,5,14}, 18 }, {  []int{1,16,1}, 0 }, {  []int{3,5,1}, 0 },
+	{  []int{8,2,12}, 10 }, {  []int{16,1,1}, 0 }, {  []int{3,3,6}, 3 }, {  []int{2,6,12}, 16 },
+	{  []int{15,1,0}, 0 }, {  []int{5,3,7}, 0 }, {  []int{4,3,5}, 0 }, {  []int{3,5,11}, 8 },
+	{  []int{7,2,13}, 16 }, {  []int{15,1,6}, 0 }, {  []int{15,1,15}, 14 }, {  []int{4,4,9}, 2 },
+	{  []int{5,3,8}, 0}, {  []int{3,5,6}, 0}, {  []int{16,1,7}, 0}, {  []int{1,15,7}, 0},
+	{  []int{4,3,12}, 17}, {  []int{5,3,13}, 14}, {  []int{2,4,5}, 2}, {  []int{5,3,5}, 0},
+	{  []int{16,1,16}, 15}, {  []int{2,5,8}, 7}, {  []int{5,3,4}, 0}, {  []int{5,3,10}, 6},
+	{  []int{4,4,7}, 0}, {  []int{3,5,9}, 3}, {  []int{4,2,2}, 0}, {  []int{4,4,15}, 20},
+	{  []int{2,2,4}, 4}, {  []int{5,3,11}, 8}, {  []int{4,4,8}, 0}, {  []int{1,16,9}, 1},
+	{  []int{4,4,16}, 24}, {  []int{1,15,6}, 0}, {  []int{15,1,8}, 0}, {  []int{5,3,6}, 0},
+	{  []int{16,1,9}, 1}, {  []int{3,5,15}, 22}, {  []int{1,15,1}, 0}, {  []int{1,15,0}, 0 },
+	{  []int{2,5,9}, 10}, {  []int{3,5,10}, 6}, {  []int{1,15,15}, 14}, {  []int{3,2,0}, 0},
+	{  []int{5,3,2}, 0}, {  []int{5,3,1}, 0}, {  []int{5,2,4}, 0}, {  []int{3,5,4}, 0},
+	{  []int{2,7,13}, 16}, {  []int{3,3,0}, 0}, {  []int{7,2,11}, 10}, {  []int{4,4,0}, 0},
+	{  []int{1,1,0}, 0}, {  []int{2,6,9}, 7}, {  []int{3,5,3}, 0}, {  []int{5,3,15}, 22},
+	{  []int{5,2,6}, 2}, {  []int{3,4,12}, 17}, {  []int{2,3,6}, 7}, {  []int{1,1,1}, 0},
+	{  []int{15,1,1}, 0}, {  []int{1,16,16}, 15}, {  []int{2,2,2}, 0}, {  []int{3,3,9}, 12},
+	{  []int{16,1,8}, 0}, {  []int{9,1,6}, 2}, {  []int{5,3,12}, 11}, {  []int{2,2,3}, 2},
+	{  []int{3,5,7}, 0}, {  []int{7,2,0}, 0}, {  []int{4,3,6}, 0}, {  []int{2,3,4}, 2},
+	{  []int{1,15,8}, 0}, {  []int{16,1,0}, 0}, {  []int{5,3,9}, 3}, {  []int{15,1,7}, 0},
+	{  []int{2,4,6}, 4}, {  []int{1,16,7}, 0}, {  []int{3,5,12}, 11}, {  []int{1,16,8}, 0},
+	{  []int{4,4,1}, 0}, {  []int{3,5,0}, 0}, {  []int{3,5,8}, 0}, {  []int{1,16,0}, 0},
+	{  []int{5,3,3}, 0}, {  []int{5,3,0}, 0}, {  []int{1,13,9}, 4}, {  []int{3,5,2}, 0},
+	{  []int{1,9,6}, 2}, {  []int{6,2,12}, 16}, {  []int{4,3,8}, 4}, {  []int{3,5,5}, 0},
+	{  []int{5,3,14}, 18}, {  []int{4,3,7}, 2}, {  []int{6,2,4}, 0}, {  []int{3,5,1}, 0},
+
+}
+func TestFindBestPermutation(t *testing.T) {
+	for _, item:= range tests {
+		fmt.Println(item)
+		permutation,count := findBestPermutation(item.values[0],item.values[1],item.values[2])
+		if count != item.result {
+			t.Error("Expected", item.result, " Got: ", count)
+		}else{
+			fmt.Println(permutation)
+		}
+	}
+}
+
+func TestCounUnheppiPoints(t *testing.T) {
+	permutation := countUnheppiPoints(3,3,"111100011")
+	//fmt.Println(permutation)
+	if permutation != 4 {
+		t.Error(permutation)
+	}
+	permutation = countUnheppiPoints(3,3,"111111111")
+	//fmt.Println(permutation)
+	if permutation != 12 {
+		t.Error(permutation)
+	}
+	permutation = countUnheppiPoints(5,5,"1111100000111111111100001")
+	//fmt.Println(permutation)
+	if permutation != 18 {
+		t.Error(permutation)
+	}
+}
+
+func TestFindAllPermutations(t *testing.T) {
+	findAllPermitations(3,3,5)
+
+}
+func TestCheckCount(t *testing.T){
+	type values struct{
+		Value string
+		Count int
+		Res bool
+	}
+	var testdata = []values{
+		{"011001110", 5, true},
+		{"011001110", 3, false},
+		{"011101110", 5, false},
+		{"011101110", 6, true},
+		{"0", 6, false},
+		{"0", 1, false},
+		{"1", 1, true},
+
+	}
+	for _, value:=range testdata{
+		res:= checkCount(value.Value, value.Count)
+		if res != value.Res {
+			t.Error(res)
+		}
+	}
+
+}
